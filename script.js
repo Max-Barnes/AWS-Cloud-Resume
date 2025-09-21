7e0c25a6f1640c126cf7eeb4864b48ed0f883048
Max Barnes <maxbarnes.dev@gmail.com>
2025-09-21 15:14:01 -0400

    sections done, projects need descriptions. Mobile view also needs work

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const skillCategories = document.querySelectorAll(".skill-category");
 const experienceButtons = document.querySelectorAll(".experience-button");
 const experienceCategories = document.querySelectorAll(".experience-category");
 
+const projectButtons = document.querySelectorAll(".project-button");
+const projectCategories = document.querySelectorAll(".project-category");
+
 // Add click event listeners to category buttons
 categoryButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -24,13 +27,25 @@ experienceButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const categoryName = button.dataset.category;
 
-    // Remove active class from all buttons and categories
     experienceButtons.forEach((btn) => btn.classList.remove("active"));
     experienceCategories.forEach((category) =>
       category.classList.remove("active")
     );
 
-    // Add active class to clicked button and corresponding category
+    button.classList.add("active");
+    document.getElementById(categoryName).classList.add("active");
+  });
+});
+
+projectButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const categoryName = button.dataset.category;
+
+    projectButtons.forEach((btn) => btn.classList.remove("active"));
+    projectCategories.forEach((category) =>
+      category.classList.remove("active")
+    );
+
     button.classList.add("active");
     document.getElementById(categoryName).classList.add("active");
   });
